1533664f89d250c97020b77cc538568d9dac2bdc
welson-rodrigues <welson.rosendo@escolar.ifrn.edu.br>
2025-01-11 22:32:46 -0300

    Movimentação do player feita

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,34 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
+const sensibilidade = 0.2
+@export var joystick : VirtualJoystick
+var input_dir
+var direction = Vector3.ZERO
 
+func _input(event):
+	if event is InputEventScreenDrag:
+		if event.position.x >= get_viewport().size.x / 2:
+			%cabeca.rotate_x(-deg_to_rad(event.relative.y) * sensibilidade)
+			rotate_y(-deg_to_rad(event.relative.x) * sensibilidade)
+			
+			%cabeca.rotation.x = clamp(%cabeca.rotation.x, deg_to_rad(-45), deg_to_rad(80))
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		
+	if joystick:
+		input_dir = joystick.output
+	else:
+		input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		
+	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), 0.5)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
