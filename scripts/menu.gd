8afb87b8b861ce983aadf7787d6e1a29a0f9b205
welson-rodrigues <welson.rosendo@escolar.ifrn.edu.br>
2025-01-21 10:56:18 -0300

    Ajustes

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,6 +3,7 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
+	#MusicController.play_music()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ scripts/music_controller.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	
+func play_music():
+	$AudioStreamPlayer2D.play()
