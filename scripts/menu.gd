91dd1d2bc221828a5713bf18e1ec618201f45dc1
welson-rodrigues <welson.rosendo@escolar.ifrn.edu.br>
2025-02-09 11:41:38 -0300

    Script entrar e sair dos créditos

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -17,3 +17,7 @@ func _on_start_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_info_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/creditos.tscn")
